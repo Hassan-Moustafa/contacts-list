65c1c7b797a0002891adc29a42aad183abf81579
Hassan Moustafa <h.moustafa@intdv.com>
2020-02-04 09:54:33 +0200

    project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   {
-    path: '', loadChildren: () => import('./modules/authentication/authentication.module').then(m => m.AuthenticationModule)
+    path: '', loadChildren: () => import('./featured/modules/authentication/authentication.module').then(m => m.AuthenticationModule)
   },
   {
-    path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+    path: 'home', loadChildren: () => import('./featured/modules/home/home.module').then(m => m.HomeModule)
   }
 ];
 
